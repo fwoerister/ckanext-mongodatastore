387c19a66e9797a173144042bd5ae538ea11c9ed
Florian Wörister <ffwoerister@gmail.com>
2020-09-29 00:47:16 +0200

    set long_description_content_type

--- setup.py
+++ setup.py
@@ -15,10 +15,11 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.1.0',
+    version='0.1.2',
 
     description='''A MongoDB DataStoreBackend implementation, that supports data citation.''',
     long_description=long_description,
+    long_description_content_type='text/markdown',
 
     # The project's main homepage.
     url='https://github.com/fwoerister/ckanext-mongodatastore',
